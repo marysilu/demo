04372ead56c8f13d9effe7c2d69e75a64a261be4
marysilu <marysilu3@gmail.com>
2022-12-21 17:46:54 +0530

    todo
    todo project

--- travelproject/travelapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import place,team
-
-
-# Create your views here.
-def display(request):
-    obj = place.objects.all()
-    obj2 = team.objects.all()
-    return render(request, 'index.html',{'result':obj,'mem':obj2})
